60b56616b722a4dc6cbed3980c610432f06cb67e
Andrew Len <ftlio@users.noreply.github.com>
2019-09-22 23:13:54 -0700

    Update shrek-bot.sh

--- app/shrek-bot.sh
+++ app/shrek-bot.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 function get_next_quote () {
-		next_quote="$(./get-shrek-quote.sh)"
-		if [ "$1" = '--to-file' ]; then
-				echo "$next_quote" > "$2"
-		else
-				echo "$next_quote"
-		fi
+	next_quote="$(./get-shrek-quote.sh)"
+	if [ "$1" = '--to-file' ]; then
+		echo "$next_quote" > "$2"
+	else
+		echo "$next_quote"
+	fi
 }
 
 if [ -n "$QUOTE_BOT_TTL" ]; then
-		for i in `seq 1 $QUOTE_BOT_TTL`
-		do
-				get_next_quote $1 $2
-				sleep 2
-		done
+	for i in `seq 1 $QUOTE_BOT_TTL`
+	do
+		get_next_quote $1 $2
+		sleep 2
+	done
 else
-		while true
-		do
-				get_next_quote $1 $2
-				sleep 2
-		done
+	while true
+	do
+		get_next_quote $1 $2
+		sleep 2
+	done
 fi
